6b4e379b6854370f208cbc8f035f8648be64030b
Jackie Nascimento <jackeline_nascimento@hotmail.com>
2025-12-13 10:15:23 -0300

    Adiciona gradle wrapper, classe principal e estrutura de testes

---
+++ src/main/java/com/jackie/devconverge/chatbotworkshop/ChatbotWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.jackie.devconverge.chatbotworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackie/devconverge/chatbotworkshop/ChatbotWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackie.devconverge.chatbotworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
